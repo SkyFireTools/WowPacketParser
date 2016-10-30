3a64f1351383c7f17406e67f106b6d302d1d0a03
AlterEgo <admin@projectskyfire.org>
2016-10-30 11:26:24 -0500

    Remove my double defined opcodes.
    Signed-off-by: AlterEgo <admin@projectskyfire.org>

--- WowPacketParser/Enums/Version/V5_4_8_18291/Opcodes.cs
+++ WowPacketParser/Enums/Version/V5_4_8_18291/Opcodes.cs
@@ -395,34 +395,7 @@ namespace WowPacketParser.Enums.Version.V5_4_8_18291
             {Opcode.CMSG_WORLD_STATE_UI_TIMER_UPDATE, 0x15AB},// SkyFire
             {Opcode.CMSG_WRAP_ITEM, 0x02DF},
             {Opcode.CMSG_CORPSE_QUERY, 0x1FBE},
-            {Opcode.MSG_LIST_STABLED_PETS, 0x02CA},
-            {Opcode.MSG_MOVE_FALL_LAND, 0x08FA},
-            {Opcode.MSG_MOVE_HEARTBEAT, 0x01F2},
-            {Opcode.MSG_MOVE_JUMP, 0x1153},
-            {Opcode.MSG_MOVE_SET_FACING, 0x1050},
-            {Opcode.MSG_MOVE_SET_PITCH, 0x017A},
-            {Opcode.MSG_MOVE_SET_RUN_MODE, 0x0979},
-            {Opcode.MSG_MOVE_SET_WALK_MODE, 0x08D1},
-            {Opcode.MSG_MOVE_START_ASCEND, 0x11FA},
-            {Opcode.MSG_MOVE_START_BACKWARD, 0x09D8},
-            {Opcode.MSG_MOVE_START_DESCEND, 0x01D1},
-            {Opcode.MSG_MOVE_START_FORWARD, 0x095A},
-            {Opcode.MSG_MOVE_START_PITCH_DOWN, 0x08D8},
-            {Opcode.MSG_MOVE_START_PITCH_UP, 0x00D8},
-            {Opcode.MSG_MOVE_START_STRAFE_LEFT, 0x01F8},
-            {Opcode.MSG_MOVE_START_STRAFE_RIGHT, 0x1058},
-            {Opcode.MSG_MOVE_START_SWIM, 0x1858},
-            {Opcode.MSG_MOVE_START_TURN_LEFT, 0x01D0},
-            {Opcode.MSG_MOVE_START_TURN_RIGHT, 0x107B},
-            {Opcode.MSG_MOVE_STOP, 0x08F1},
-            {Opcode.MSG_MOVE_STOP_ASCEND, 0x115A},
-            {Opcode.MSG_MOVE_STOP_PITCH, 0x007A},
-            {Opcode.MSG_MOVE_STOP_STRAFE, 0x0171},
-            {Opcode.MSG_MOVE_STOP_SWIM, 0x0950},
-            {Opcode.MSG_MOVE_STOP_TURN, 0x1170},
-            {Opcode.MSG_MOVE_WORLDPORT_ACK, 0x1FAD},
-            {Opcode.MSG_QUERY_NEXT_MAIL_TIME, 0x089B},
-            {Opcode.MSG_SET_RAID_DIFFICULTY, 0x0591},
+            {Opcode.MSG_LIST_STABLED_PETS, 0x02CA},            
         };
 
   private static readonly BiDictionary<Opcode, int> ServerOpcodes = new BiDictionary<Opcode, int>
@@ -712,7 +685,7 @@ namespace WowPacketParser.Enums.Version.V5_4_8_18291
             {Opcode.MSG_MOVE_START_STRAFE_LEFT, 0x01F8},
             {Opcode.MSG_MOVE_START_STRAFE_RIGHT, 0x1058},
             {Opcode.MSG_MOVE_START_SWIM, 0x1858},
-            {Opcode.MSG_MOVE_START_TURN_LEFT,0x01D0},
+            {Opcode.MSG_MOVE_START_TURN_LEFT, 0x01D0},
             {Opcode.MSG_MOVE_START_TURN_RIGHT, 0x107B},
             {Opcode.MSG_MOVE_STOP, 0x08F1},
             {Opcode.MSG_MOVE_STOP_ASCEND, 0x115A},
@@ -721,6 +694,8 @@ namespace WowPacketParser.Enums.Version.V5_4_8_18291
             {Opcode.MSG_MOVE_STOP_SWIM, 0x0950},
             {Opcode.MSG_MOVE_STOP_TURN, 0x1170},
             {Opcode.MSG_MOVE_WORLDPORT_ACK, 0x1FAD},
+            {Opcode.MSG_QUERY_NEXT_MAIL_TIME, 0x089B},
+            {Opcode.MSG_SET_RAID_DIFFICULTY, 0x0591},
             {Opcode.MSG_VERIFY_CONNECTIVITY, 0x4F57}
         };
  }
